be5f9d48d798796c7d23e1fe26dcb96d531f73d1
CODE BRAIN <dorjitshering370@gmail.com>
2021-02-26 21:57:38 -0800

    fourth

---
+++ 7.py
@@ -0,0 +1,47 @@
+# game of two stacks----ultimate logic required
+gameCount = int(input())
+first = input()
+second = input()
+third = input()
+
+# variable extraction
+first_ = first.split(' ')
+second_ = second.split(' ')
+third_ = third.split(' ')
+
+data = list(map(lambda x: int(x), first_))
+A = list(map(lambda x: int(x), second_))
+B = list(map(lambda x: int(x), third_))
+
+length_A = data[0]
+length_B = data[1]
+limit = data[2]
+
+# game function
+def gameOfStacks(g, x, a, b):        
+    for i in range(g):
+        i = 0
+        j = 0
+        count = 0
+        sum_ = 0
+    # find the count from the first stack 
+        while(i<len(a) and sum_+a[i] <= x):
+            sum_ += a[i]          
+            i += 1
+        count = i
+    # add each element from the second stack to the last sum from the first stack
+    # if it exceeds x, subtract the latest removed element from first stack
+    # i+j will give you the exact count of how many integers are involved in the sum
+        while (j<len(b) and i>=0):
+            sum_ += b[j]
+            j += 1
+            while(sum_ > x and i>0):
+                i -= 1
+                sum_ -= a[i]
+        if(i+j > count and sum_ <= x):
+            count = i+j
+        print(count)  
+gameOfStacks(gameCount, limit, A, B)
+
+
+
--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Question3: https://www.hackerrank.com/challenges/array-left-rotation/problem
 Question4: https://www.hackerrank.com/challenges/sparse-arrays/problem
 Question5: https://www.hackerrank.com/challenges/waiter/problem
 Question6: https://www.hackerrank.com/challenges/queue-using-two-stacks/problem
+Question7: https://www.hackerrank.com/challenges/game-of-two-stacks/problem
