25170f905a7ec1a66aab21be5fad57de5073567e
CODE BRAIN <dorjitshering370@gmail.com>
2021-02-27 23:28:15 -0800

    comments added

--- 9.js
+++ 9.js
@@ -6,12 +6,14 @@ function arrayManipulation(n, queries) {
     for(let i=0; i<=n; i++){
         array[i] = 0;
     }
-    // manipulate with queries
+    // just set the values of the endpoints, a and b+1(-ve) for optimization
     for(let j=0; j<queries.length; j++){
         array[(queries[j][0])] += queries[j][2];
         array[queries[j][1]+1] -= queries[j][2];
     }
     
+    // add all positive(left) endpoints of each query in the array 
+    // then store the greatest of max and sum in the max variable
     var max = 0;
     var sum = 0;
     for(let i=1; i<=n; i++){        
